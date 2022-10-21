c1993c1752378183b4f878a2aefded2830d5ac42
Ross Miller <ross.miller@digital.trade.gov.uk>
2022-10-21 14:33:44 +0100

    Add auth to all views

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,5 +140,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
