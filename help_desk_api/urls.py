1e641fa75a7794931019614fb5ec183c47aedb3b
Ross Miller <ross.miller@digital.trade.gov.uk>
2022-10-30 11:10:48 +0000

    Add zendesk proxy and team app

--- help_desk_api/urls.py
+++ help_desk_api/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
 
-from help_desk_api.views import TicketView
+from help_desk_api.views import CommentView, MeView, TicketView, UserView
+
+"""
+zenpy.Zenpy.tickets.create - POST /api/v2/tickets
+zenpy.Zenpy.tickets - GET /api/v2/tickets/{id}
+zenpy.Zenpy.tickets.update - PUT /api/v2/tickets/{id}
+zenpy.Zenpy.tickets.comments - GET /api/v2/tickets/{id}/comments
+zenpy.Zenpy.users - GET /api/v2/users/{id}
+zenpy.Zenpy.users.create_or_update - POST /api/v2/users/create_or_update
+zenpy.Zenpy.users.me - GET /api/v2/users/me
+"""
 
 urlpatterns = [
-    path("v2/tickets.json", TicketView.as_view(), name="ticket"),
+    path("v2/tickets.json", TicketView.as_view(), name="tickets"),
+    path("v2/tickets/<int:id>.json", TicketView.as_view(), name="ticket"),
+    path("v2/tickets/<int:id>/comments.json", CommentView.as_view(), name="comments"),
+    path("v2/users/<int:id>.json", UserView.as_view(), name="user"),
+    path("v2/users/create_or_update.json", UserView.as_view(), name="crete_user"),
+    path("v2/users/me.json", MeView.as_view(), name="me"),
 ]
--- help_desk_api/views.py
+++ help_desk_api/views.py
@@ -2,7 +2,56 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from help_desk_api.serializers import TicketContainer
-from help_desk_api.utils import http_verb
+
+
+class UserView(APIView):
+    """
+    View for interaction with tickets
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a ticket
+        """
+        # Get ticket from Halo
+
+        return Response()
+
+    def post(self, request, format=None):
+        """
+        Return a ticket
+        """
+        # Get ticket from Halo
+
+        return Response()
+
+
+class MeView(APIView):
+    """
+    View for interaction with tickets
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a ticket
+        """
+        # Get ticket from Halo
+
+        return Response()
+
+
+class CommentView(APIView):
+    """
+    View for interaction with tickets
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a ticket
+        """
+        # Get ticket from Halo
+
+        return Response()
 
 
 class TicketView(APIView):
@@ -10,7 +59,27 @@ class TicketView(APIView):
     View for interaction with tickets
     """
 
-    @http_verb(verb="POST")
+    def get(self, request, id, format=None):
+        """
+        Return a ticket
+        """
+        # Get ticket from Halo
+
+        # snippet = self.get_object(pk)
+        # serializer = SnippetSerializer(snippet)
+        # return Response(serializer.data)
+
+        return Response()
+
+    def put(self, request, id, format=None):
+        # snippet = self.get_object(pk)
+        # serializer = SnippetSerializer(snippet, data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response()
+
     def post(self, request, format=None):
         """
         Create a ticket
