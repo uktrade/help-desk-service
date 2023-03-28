b323fc06ecaeee9d5c852723620f89bd288af8ee
NickFitz <nick@nickfitz.co.uk>
2023-03-28 17:09:02 +0100

    Custom `ModelAdmin` for handling token encryption via admin forms.

--- help_desk_api/admin.py
+++ help_desk_api/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from help_desk_api.models import HelpDeskCreds
 
-admin.site.register(HelpDeskCreds)
+from .forms import HelpDeskCredsChangeForm, HelpDeskCredsCreationForm
+
+
+@admin.register(HelpDeskCreds)
+class HelpDeskCredsAdmin(admin.ModelAdmin):
+    form = HelpDeskCredsChangeForm
+    add_form = HelpDeskCredsCreationForm
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special form during credentials creation
+        This is lifted from django.contrib.auth.admin.UserAdmin  /PS-IGNORE
+        """
+        defaults = {}
+        if obj is None:
+            defaults["form"] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
---
+++ help_desk_api/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from help_desk_api.models import HelpDeskCreds
+
+
+class HelpDeskCredsChangeForm(forms.ModelForm):
+    zendesk_token = ReadOnlyPasswordHashField(
+        label="Zendesk token",
+        help_text="Raw tokens are not stored, so there is no way to see this "
+        "token, but you can change the token using "
+        '<a href="{}">this form</a>.',
+    )
+
+    class Meta:
+        model = HelpDeskCreds
+        fields = (
+            "zendesk_email",
+            "zendesk_token",
+            "zendesk_subdomain",
+            "halo_client_id",
+            "halo_client_secret",
+            "help_desk",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        zendesk_token = self.fields.get("zendesk_token")
+        if zendesk_token:
+            zendesk_token.help_text = zendesk_token.help_text.format("../zendesk_token/")
+
+
+class HelpDeskCredsCreationForm(forms.ModelForm):
+    class Meta:
+        model = HelpDeskCreds
+        fields = (
+            "zendesk_email",
+            "zendesk_token",
+            "zendesk_subdomain",
+            "halo_client_id",
+            "halo_client_secret",
+            "help_desk",
+        )
+
+    def save(self, commit=True):
+        credentials: HelpDeskCreds = super().save(commit=False)
+        credentials.set_token(self.cleaned_data["zendesk_token"])
+        if commit:
+            credentials.save()
+        return credentials
