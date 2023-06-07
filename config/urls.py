d2bc0faef5ce2e2612e106a962c06bec91b9be75
NickFitz <nick@nickfitz.co.uk>
2023-06-07 09:48:24 +0100

    Rearrange API URL configs to make them easier to include in the schema view arguments.

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from help_desk_api import urls as help_desk_api_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("help_desk_api.urls", namespace="api")),
+    path("", include(help_desk_api_urls, namespace="api")),
 ]
