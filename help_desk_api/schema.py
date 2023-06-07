69f50f0ca8b6ca59e2983cb025d7e87222f4044c
NickFitz <nick@nickfitz.co.uk>
2023-06-07 09:53:31 +0100

    Schema generator that grants permission to introspect all views.
    This is necessary because our API views all use the custom Zenpy authentication, and there isn't any obvious method to authenticate the schema-generating views that way.

---
+++ help_desk_api/schema.py
@@ -0,0 +1,16 @@
+from rest_framework.schemas.openapi import SchemaGenerator
+
+
+class FullDisclosureSchemaGenerator(SchemaGenerator):
+    """
+    Allows OpenAPI schema generator views to access all views.
+
+    The default implementation only examines APIViews which
+    the client requesting the OpenAPI schema has permission to access.
+    All API endpoints use help_desk_api.auth.ZenpyAuthentication, but
+    it is not practical to require the client to authenticate
+    in that way to view the schema. Therefore, permit access to all views.
+    """
+
+    def has_view_permissions(self, path, method, view):
+        return True
