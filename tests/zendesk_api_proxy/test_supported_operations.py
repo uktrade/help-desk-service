9b1ac3c1c9319485326a76b3025c456c876b7c8b
nikosbalta <nikos.baltas@digital.trade.gov.uk>
2023-06-20 09:59:41 +0100

    Unit Testing for GET request TS2-106

--- tests/zendesk_api_proxy/test_supported_operations.py
+++ tests/zendesk_api_proxy/test_supported_operations.py
@@ -15,7 +15,7 @@ class TestSupportedOperations:
     '''
 
     @mock.patch("zendesk_api_proxy.middleware.proxy_zendesk")
-    def test_update_ticket_by_id(
+    def test_get_ticket_by_id(
         self,
         proxy_zendesk: mock.MagicMock, 
         client: Client,
@@ -23,17 +23,6 @@ class TestSupportedOperations:
         zendesk_creds_only,
         zendesk_authorization_header
     ):
-        ticket_id = 123
-        created_at = '2018-01-28T14:50:32Z'
-        subject = 'Any subject'
-        priority = 'Low'
-        status = 'pending'
-        requester_id = 18655244625
-
-        data = {
-            "description": "A description",
-        }
-
         url = reverse("api:ticket", kwargs={"id": 123})
         
         response = Response()
@@ -41,17 +30,19 @@ class TestSupportedOperations:
         response.status_code = HTTPStatus.OK
         
         proxy_zendesk.return_value = response
-        
-        client.put(url, data=data, headers={"Authorization": zendesk_authorization_header})
-        proxy_zendesk.assert_called_once() # make sure the middleware called the function
-        
-        client.get(url, headers={"Authorization": zendesk_authorization_header})
-        proxy_zendesk.assert_called
+        res1=client.get(url, headers={"Authorization": zendesk_authorization_header})
+        proxy_zendesk.assert_called_once()
 
-        request = proxy_zendesk.call_args[0]
+        request = proxy_zendesk.call_args[0]  # args passed to proxy_zendesk
+        # request[0] : HttpRequest object
+        # request[1] : Zendesk subdomain
+        # request[2] : email address extracted from the Auth header
+        # request[3] : token
         
-        
-        #assert request.url == url # not sure about this - look up Request docs
-        #assert request.data == data
-        #assert request.method == "PUT"
+        assert request[1] == zendesk_creds_only.zendesk_subdomain 
+        assert request[2] == zendesk_creds_only.zendesk_email
+        #assert request[3] == zendesk_creds_only.zendesk_token
+        assert request[0].get_full_path() == url
+        assert request[0].method == "GET"
+
       
