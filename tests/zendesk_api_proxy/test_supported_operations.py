a8c3ebe86342937054d8fb7a1730062be52f298e
nikosbalta <nikos.baltas@digital.trade.gov.uk>
2023-06-21 13:04:58 +0100

    Unit testing GET and PUT on zendesk_proxy

---
+++ tests/zendesk_api_proxy/test_supported_operations.py
@@ -0,0 +1,79 @@
+from unittest import mock
+import pytest
+
+from django.test import Client
+from django.urls import reverse
+from http import HTTPStatus
+from requests import Response
+
+
+class TestSupportedOperations:
+    '''
+    The following @mock.patch lines ensure the external services don't actually
+    have requests made to them by the test.
+    '''
+
+    @mock.patch("zendesk_api_proxy.middleware.proxy_zendesk")
+    def test_get_ticket_by_id(
+        self,
+        proxy_zendesk: mock.MagicMock,
+        client: Client,
+        zendesk_required_settings, # fixture: see /tests/conftest.py
+        zendesk_creds_only,
+        zendesk_authorization_header
+    ):
+        url = reverse("api:ticket", kwargs={"id": 123})
+
+        response = Response()
+        response._content = b"{}"
+        response.status_code = HTTPStatus.OK
+
+        proxy_zendesk.return_value = response
+        client.get(url, headers={"Authorization": zendesk_authorization_header})
+        proxy_zendesk.assert_called_once()
+
+        request = proxy_zendesk.call_args[0]  # args passed to proxy_zendesk
+        # request[0] : HttpRequest object
+        # request[1] : Zendesk subdomain
+        # request[2] : email address extracted from the Auth header
+        # request[3] : token
+
+        assert request[0].get_full_path() == url
+        assert request[0].method == "GET"
+        assert request[1] == zendesk_creds_only.zendesk_subdomain
+        assert request[2] == zendesk_creds_only.zendesk_email
+
+
+    @mock.patch("zendesk_api_proxy.middleware.proxy_zendesk")
+    def test_post_ticket(
+        self,
+        proxy_zendesk: mock.MagicMock,
+        client: Client,
+        zendesk_required_settings, # fixture: see /tests/conftest.py
+        zendesk_creds_only,
+        zendesk_authorization_header
+    ):
+        data = {
+            "description": "A description",
+        }
+
+        url = reverse("api:tickets")
+
+        response = Response()
+        response._content = b"{}"
+        response.status_code = HTTPStatus.OK
+
+        proxy_zendesk.return_value = response
+        client.post(url, headers={"Authorization": zendesk_authorization_header})
+        proxy_zendesk.assert_called_once()
+
+        request = proxy_zendesk.call_args[0]  # args passed to proxy_zendesk
+        # request[0] : HttpRequest object
+        # request[1] : Zendesk subdomain
+        # request[2] : email address extracted from the Auth header
+        # request[3] : token
+
+        assert request[0].get_full_path() == url
+        assert request[0].method == "POST"
+        assert request[1] == zendesk_creds_only.zendesk_subdomain
+        assert request[2] == zendesk_creds_only.zendesk_email
