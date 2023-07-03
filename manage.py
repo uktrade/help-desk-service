e19f42f8e33eb623ac6ce6ae6a06bf307a2814ef
Sravan Jinna <sravan.jinna@digital.trade.gov.uk>
2023-07-03 10:45:28 +0100

    more refactoring to Pagination and added unittests

--- manage.py
+++ manage.py
@@ -3,18 +3,10 @@
 import os
 import sys
 
-from django.conf import settings
-
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
-    if settings.DEBUG:
-        if os.environ.get("RUN_MAIN"):
-            import debugpy
-
-            debugpy.listen(("0.0.0.0", 3000))
-            print("attached")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
