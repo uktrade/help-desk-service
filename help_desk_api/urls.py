168874c97f0f3208ad3317ddcc19155ac06f2bcd
Nikos Baltas <78555365+nikosbalta@users.noreply.github.com>
2023-07-25 12:59:04 +0100

    Feature/ts2 154 implement attachments logic in zendesk proxy (#21)
    * added path to uploads
    
    * image file for testing uploads
    
    * added UploadView class for Halo
    
    * added tests for uploading attachment
    
    * modified content_type settings
    
    * tidying up
    
    * tidying up

--- help_desk_api/urls.py
+++ help_desk_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from help_desk_api.views import CommentView, MeView, TicketView, UserView
+from help_desk_api.views import CommentView, MeView, TicketView, UploadsView, UserView
 
 app_name = "help_desk_api"
 
@@ -12,6 +12,7 @@ zenpy.Zenpy.tickets.comments - GET /api/v2/tickets/{id}/comments
 zenpy.Zenpy.users - GET /api/v2/users/{id}
 zenpy.Zenpy.users.create_or_update - POST /api/v2/users/create_or_update
 zenpy.Zenpy.users.me - GET /api/v2/users/me
+zenpy.Zenpy.uploads - POST /api/v2/uploads
 """
 
 urlpatterns = [
@@ -25,6 +26,7 @@ urlpatterns = [
                 path("v2/users/<int:id>.json", UserView.as_view(), name="user"),
                 path("v2/users/create_or_update.json", UserView.as_view(), name="crete_user"),
                 path("v2/users/me.json", MeView.as_view(), name="me"),  # /PS-IGNORE
+                path("v2/uploads.json", UploadsView.as_view(), name="uploads"),  # /PS-IGNORE
             ]
         ),
     )
