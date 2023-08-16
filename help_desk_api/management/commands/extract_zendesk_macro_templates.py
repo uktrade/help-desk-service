296196b28e6fbbf4602dde60b9b3baf799f4b6aa
NickFitz <nick@nickfitz.co.uk>
2023-08-16 09:51:23 +0100

    Use a base command class to generalise the fetching of Zendesk data.

--- help_desk_api/management/commands/extract_zendesk_macro_templates.py
+++ help_desk_api/management/commands/extract_zendesk_macro_templates.py
@@ -10,18 +10,21 @@ from .zendesk_data_base_command import ZendeskDataBaseCommand
 
 class Command(ZendeskDataBaseCommand):
     help = "Extract macro templates from Zendesk JSON"  # /PS-IGNORE
+    api_response_content_field = "macros"
+    api_start_url_path = "/api/v2/macros.json"
 
     def handle(self, *args, **options):
         macro_data = self.load_zendesk_data(
-            credentials=options["credentials"], file_path=options["inputfile"]
+            token=options["zendesktoken"],
+            credentials=options["credentials"],
+            file_path=options["inputfile"],
         )
 
         macros = ZenpyMacros(macro_data)
-
-        content = {
-            "plaintext_comments": macros.plaintext_comments,
-            "html_comments": macros.html_comments,
+        output = {
             "subjects": macros.subjects,
+            "html": macros.with_html_comments,
+            "text": macros.with_plaintext_comments,
         }
 
         if options["output"]:
@@ -31,6 +34,6 @@ class Command(ZendeskDataBaseCommand):
             output_path = settings.BASE_DIR / output_path
             output_path.parent.mkdir(parents=True, exist_ok=True)
             with open(output_path, "w") as output_file:
-                json.dump(content, output_file, indent=4)
+                json.dump(output, output_file, indent=4)
         else:
-            json.dump(content, self.stdout, indent=4)
+            json.dump(output, self.stdout, indent=4)
