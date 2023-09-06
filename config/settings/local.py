f6a6cfc918871fc13e3f7278634bfd9929887c18
Sravan Jinna <sravan.jinna@digital.trade.gov.uk>
2023-09-06 14:49:49 +0100

    added sentry logging in local aka staging

--- config/settings/local.py
+++ config/settings/local.py
@@ -10,9 +10,3 @@ REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
 ]
 REST_FRAMEWORK["DEFAULT_PAGINATION_CLASS"] = "help_desk_api.pagination.CustomPagination"
 REST_FRAMEWORK["PAGE_SIZE"] = 100
-
-sentry_sdk.init(
-    os.environ.get("SENTRY_DSN"),
-    environment=os.environ.get("SENTRY_ENVIRONMENT"),
-    integrations=[DjangoIntegration()],
-)
