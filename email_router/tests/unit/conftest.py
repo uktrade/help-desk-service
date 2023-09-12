63d38bdf93f2c8c83ce2965607f59ec1a08232c3
NickFitz <nick@nickfitz.co.uk>
2023-09-12 17:14:54 +0100

    Add upload response fixture for testing.

--- email_router/tests/unit/conftest.py
+++ email_router/tests/unit/conftest.py
@@ -1,9 +1,11 @@
+import json
 from email import policy
 from email.parser import BytesParser
 from pathlib import Path
 
 import pytest
 from email_router.ses_email_receiving.email_utils import ParsedEmail
+from requests import Response
 
 
 @pytest.fixture(scope="session")
@@ -21,3 +23,12 @@ def email_message(email_bytes):
 @pytest.fixture(scope="session")
 def parsed_email(email_bytes):
     return ParsedEmail(raw_bytes=email_bytes)
+
+
+@pytest.fixture(scope="session")
+def zendesk_upload_response():
+    response = Response()
+    response_dict = {"upload": {"token": "1234"}}
+    response_json = json.dumps(response_dict)
+    response._content = bytes(response_json, "utf-8")
+    return response
