11ddf38f4827fc5c358325d7842c92886291ae99
NickFitz <nick@nickfitz.co.uk>
2024-01-26 12:02:09 +0000

    Fix typo in URL config name.

--- help_desk_api/urls.py
+++ help_desk_api/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
                 path("v2/tickets/<int:id>.json", TicketView.as_view(), name="ticket"),
                 path("v2/tickets/<int:id>/comments.json", CommentView.as_view(), name="comments"),
                 path("v2/users/<int:id>.json", UserView.as_view(), name="user"),
-                path("v2/users/create_or_update.json", UserView.as_view(), name="crete_user"),
+                path("v2/users/create_or_update.json", UserView.as_view(), name="create_user"),
                 path("v2/users/me.json", MeView.as_view(), name="me"),  # /PS-IGNORE
                 path("v2/uploads.json", UploadsView.as_view(), name="uploads"),  # /PS-IGNORE
             ]
