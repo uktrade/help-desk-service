d24e7c178c4548c2ff1536f5e9577ed8cc18fc31
NickFitz <nick@nickfitz.co.uk>
2024-02-20 14:18:44 +0000

    Ensure `audit` object present in Halo-to-Zendesk ticket container.

--- tests/halo_api/test_halo_to_zendesk_transformation.py
+++ tests/halo_api/test_halo_to_zendesk_transformation.py
@@ -23,12 +23,27 @@ class TestHaloToZendeskTicketSerialisation:
     def test_zendesk_ticket_container_is_not_array(self, new_halo_ticket):
         # Halo Client wraps the ticket data as a Zendesk response
         serializer = HaloToZendeskTicketContainerSerializer(new_halo_ticket)
-        # serializer.is_valid(raise_exception=False)
+
         zendesk_equivalent = serializer.data
 
         assert isinstance(zendesk_equivalent, dict)
         assert "ticket" in zendesk_equivalent
         assert isinstance(zendesk_equivalent["ticket"], dict)
+
+    def test_zendesk_ticket_container_includes_audit(self, new_halo_ticket):
+        """
+        Zendesk ticket updates include an "audit" object alongside the ticket.
+        The presence of the audit changes the behaviour of Zenpy,
+        so we need to be sure it's present.
+        """
+        # Halo Client wraps the ticket data as a Zendesk response
+        serializer = HaloToZendeskTicketContainerSerializer(new_halo_ticket)
+
+        zendesk_equivalent = serializer.data
+
+        assert isinstance(zendesk_equivalent, dict)
+        assert "audit" in zendesk_equivalent
+        assert isinstance(zendesk_equivalent["audit"], dict)
 
     def test_halo_details_is_zendesk_description(self, new_halo_ticket):
         serializer = HaloToZendeskTicketSerializer(new_halo_ticket)
