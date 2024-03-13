47ac58f05175aa8c2aab102a75f1db1d65ce8b97
NickFitz <nick@nickfitz.co.uk>
2024-03-13 11:02:49 +0000

    Switch to invocation from SQS.

--- email_router/ses_email_receiving/app.py
+++ email_router/ses_email_receiving/app.py
@@ -4,18 +4,17 @@ from datetime import datetime
 from urllib.parse import unquote_plus
 
 import boto3
-from aws_lambda_powertools.utilities.data_classes import event_source
-from aws_lambda_powertools.utilities.data_classes.s3_event import S3Event
+from aws_lambda_powertools.utilities.data_classes import S3Event, event_source
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSEvent, SQSRecord
 
 from .email_utils import APIClient, ParsedEmail
 
 aws_session_token = os.environ.get("AWS_SESSION_TOKEN")  # /PS-IGNORE
 
 
-@event_source(data_class=S3Event)
-def lambda_handler(event: S3Event, context):
+@event_source(data_class=SQSEvent)
+def lambda_handler(event: SQSEvent, context):
     iso_utcnow = get_iso_utcnow()
-    bucket_name = event.bucket_name
     parameters = get_parameters()
 
     api_client = APIClient(
@@ -25,8 +24,14 @@ def lambda_handler(event: S3Event, context):
     )
 
     emails = []
+    s3_events = []
+    bucket_name = ""
+    record: SQSRecord
     for record in event.records:
-        object_key = unquote_plus(record.s3.get_object.key)
+        s3_event: S3Event = record.decoded_nested_s3_event
+        s3_events.append(s3_event)
+        bucket_name = s3_event.bucket_name
+        object_key = unquote_plus(s3_event.object_key)
         email_content = get_email_from_bucket(bucket_name, object_key)
         parsed_email = ParsedEmail(raw_bytes=email_content)
         api_client.create_ticket_from_message(parsed_email)
@@ -36,19 +41,24 @@ def lambda_handler(event: S3Event, context):
 
     status = {
         "statusCode": 200,
+    }
+
+    logged_output = {
+        "status": status,
         "session_token": aws_session_token,
         "outputFilename": output_filename,
         "datetime": iso_utcnow,
         "emails": [str(email) for email in emails],
         "parameters": parameters,
         "event": event.raw_event,
+        "s3_events": s3_events,
     }
 
     s3 = boto3.client("s3")
     s3.put_object(
         Bucket=bucket_name,
         Key=output_filename,
-        Body=json.dumps(status, indent=4),
+        Body=json.dumps(logged_output, indent=4),
     )
     return status
 
