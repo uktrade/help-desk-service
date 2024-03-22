848eec1db1e666d1f3616885e7b83f8b70cd6044
NickFitz <nick@nickfitz.co.uk>
2024-03-22 16:34:22 +0000

    Test for `AuthenticationFailed` being raised when invalid credentials are provided in the request.

--- tests/zendesk_api_proxy/test_middleware.py
+++ tests/zendesk_api_proxy/test_middleware.py
@@ -10,6 +10,7 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpRequest, HttpResponse
 from django.test import Client, RequestFactory
 from django.urls import reverse
+from rest_framework.exceptions import AuthenticationFailed
 from zendesk_api_proxy.middleware import ZendeskAPIProxyMiddleware
 
 from help_desk_api.models import HelpDeskCreds
@@ -489,3 +490,52 @@ class TestZendeskResponseIDsPersisted:
         zendesk_data_passed_to_halo_manager = mock_halo_manager_create_ticket.call_args.args[0]
         assert "zendesk_ticket_id" in zendesk_data_passed_to_halo_manager
         assert zendesk_data_passed_to_halo_manager["zendesk_ticket_id"] is None
+
+
+class TestCredentials:
+    """/PS-IGNORE
+    The credentials system relies on an email address and Zendesk token
+    encoded in a request's Authorization header.
+    The header is deconstructed to obtain the values of these
+    and confirm that a corresponding record is present
+    as a HelpDeskCreds object.
+    If such a record exists, these credentials are then used directly
+    to connect to the Zendesk API.
+    For Halo, the Client ID and Secret are held
+    in the HelpDeskCreds object.
+    """
+
+    def test_get_zenpy_request_vars(
+        self, rf: RequestFactory, zendesk_authorization_header: str, zendesk_email, zendesk_token
+    ):
+        request = rf.get(reverse("api:tickets"), HTTP_AUTHORIZATION=zendesk_authorization_header)
+        from help_desk_api.utils import get_zenpy_request_vars
+
+        token, email = get_zenpy_request_vars(request)
+
+        assert token == zendesk_token
+        assert email == zendesk_email
+
+    def test_unknown_email_address_raises_401_unauthorized(
+        self, rf: RequestFactory, unknown_email_zendesk_authorization_header: str
+    ):
+        request = rf.get(
+            reverse("api:tickets"), HTTP_AUTHORIZATION=unknown_email_zendesk_authorization_header
+        )
+        mock_get_response = MagicMock()
+        middleware = ZendeskAPIProxyMiddleware(mock_get_response)
+
+        with pytest.raises(AuthenticationFailed):
+            middleware(request)
+
+    def test_incorrect_token_raises_401_unauthorized(
+        self, rf: RequestFactory, incorrect_token_zendesk_authorization_header: str
+    ):
+        request = rf.get(
+            reverse("api:tickets"), HTTP_AUTHORIZATION=incorrect_token_zendesk_authorization_header
+        )
+        mock_get_response = MagicMock()
+        middleware = ZendeskAPIProxyMiddleware(mock_get_response)
+
+        with pytest.raises(AuthenticationFailed):
+            middleware(request)
