dac82ebc2d8806771c59f924f769621ff7a7c849
NickFitz <nick@nickfitz.co.uk>
2024-03-26 16:07:05 +0000

    Filter handled exceptions. Sentry logs them anyway, and we don't want that as it's very annoying to see a handled 401 logged every time you sue the admin.

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,8 +3,28 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 from config.settings.base import *  # type: ignore # noqa
 
+
+def strip_handled_exceptions(event, _hint):
+    # Ignore logged errors
+    if "logger" in event:
+        return None
+
+    # Ignore handled exceptions
+    exceptions = event.get("exception", {}).get("values", [])
+    if exceptions:
+        exc = exceptions[-1]
+        mechanism = exc.get("mechanism")
+
+        if mechanism:
+            if mechanism.get("handled"):
+                return None
+
+    return event
+
+
 sentry_sdk.init(
     os.environ.get("SENTRY_DSN"),
     environment=os.environ.get("SENTRY_ENVIRONMENT"),
     integrations=[DjangoIntegration()],
+    before_send=strip_handled_exceptions,
 )
