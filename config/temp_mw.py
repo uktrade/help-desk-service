5b5ef72e9a5badc031e9a98fdeb342a4a4b81f6b
NickFitz <nick@nickfitz.co.uk>
2024-04-15 14:51:46 +0100

    Log all of META.

--- config/temp_mw.py
+++ config/temp_mw.py
@@ -6,6 +6,8 @@ def temp_middleware(get_response):
         header, value = settings.SECURE_PROXY_SSL_HEADER
         header_value = request.META.get(header)
         print(f"META: {header_value}")
+        for key, value in request.META.items():
+            print(f"{key}: {value}")
         return get_response(request)
 
     return mw
